836033cc5e7450503982187f6233467335d6dbea
Sharnell Grant <sharnellgrant@gmail.com>
2020-05-16 01:19:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < Sinatra::Base
    @user = User.find_by(username: params[:username], password: params[:password])
     if @user
       session[:user_id] = @user.id
-      binding.pry
       redirect to '/account'
     else
       erb :error
